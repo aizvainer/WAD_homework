df17aaa7f33a4dc6823a057ca6fb911649bf1ea8
DianaPri <diana980104@gmail.com>
2023-11-26 16:36:35 +0200

    SignUpView updated. Added PasswordConditionsModal in components

---
+++ src/components/PasswordConditionsModal.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="modal" v-if="showModal">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <h2>Password Conditions</h2>
+      <ul>
+        <li>At least one uppercase letter</li>
+        <li>At least two lowercase letters</li>
+        <li>At least one numeric value</li>
+        <li>Start with an uppercase letter</li>
+        <li>Include the character "_"</li>
+        <li>Length should be at least 8 characters and less than 15 characters</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
+  },
+};
+</script>
+
+<style>
+
+.modal {
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  border-radius: 20px;
+  width: 50%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+</style>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,18 +1,152 @@
 <template>
     <div>
-        SIgn up Page
-    </div>
+        <HeaderComponent />
+            <div class="container">
+                <div class="signUpContainer">
+                  <h2>Create an account</h2>
+                    <form @submit.prevent="submitForm" class="userForm">
+                        
+                        <div class="formGroup">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" v-model="email" placeholder="Email" required />
+                        </div>
+
+                         <div class="formGroup">
+                            <label for="password">Password</label>
+                            <input type="password" id="password" v-model="password" placeholder="Password" required/>
+                            
+                        </div>
+
+                        <div v-if="!isPasswordValid" class="passwordValidation">
+                            Password is not valid.
+                        </div>
+
+                        <button type="submit" class="signUpButton">Sign Up</button>
+                    </form>
+                </div>
+            </div>
+            <PasswordConditionsModal ref="conditionsModal" />
+        </div>
+        <FooterComponent />
 </template>
 
 <script>
 import HeaderComponent from '@/components/HeaderComponent.vue'
+import PasswordConditionsModal from '@/components/PasswordConditionsModal.vue'
 import FooterComponent from '@/components/FooterComponent.vue'
 
 export default {
     name: 'SignUpView',
     components: {
     HeaderComponent,
+    PasswordConditionsModal,
     FooterComponent
-  }
-}
+  },
+
+data() {
+    return {
+      email: "",
+      password: "",
+      isPasswordValid: true,
+    };
+  },
+
+  methods: {
+    validatePassword() {
+      const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z].*[a-z])(?=.*\d)(?=.*_)[A-Z][A-Za-z\d_]{7,14}$/;
+      this.isPasswordValid = passwordRegex.test(this.password);
+    },
+
+    submitForm() {
+      this.validatePassword();
+      if (!this.isPasswordValid) {
+        this.$refs.conditionsModal.openModal();
+        return;
+      }
+  
+    },
+  },
+};
+
 </script>
+
+<style>
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f0f0f0;
+}
+
+.signUpContainer {
+    height: 350px;
+    width: 500px;
+    background-color: #ffffff; 
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+h2 {
+  margin-top: 20px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.userForm {
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  padding: 30px;
+  margin-top: 10px;
+  margin-right: 75px;
+
+}
+
+.formGroup {
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+}
+
+label {
+  margin-bottom: 5px;
+  margin-right: 20px;
+}
+
+input {
+  text-align: center;
+  margin-bottom: 10px;
+  padding: 8px;
+  border-radius: 10px;
+  background-color: #f9f9fa;
+  border: 1px solid #ccc;
+}
+
+.passwordValidation {
+  color: red;
+  font-size: 15px;
+  margin-bottom: 10px;
+  margin-right: 40px;
+}
+
+.signUpButton {
+  padding: 10px;
+  width: 100px;
+  height: 35px;
+  margin-right: 70px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 25px;
+  cursor: pointer;
+}
+
+button:hover{
+  background-color: #0056b3;
+}
+
+</style>
